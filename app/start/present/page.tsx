024aa6552db9c6996240d14003a260ece2c3efe9
Daryl Kevin <dkevin77@icloud.com>
2025-04-15 10:32:29 +0800

    test file

--- app/start/present/page.tsx
+++ app/start/present/page.tsx
@@ -58,11 +58,11 @@ export default function Page() {
   };
 
   const handleFile = (file) => {
-    if (file && file.type === "application/pdf") {
+    if (file) {
       const url = URL.createObjectURL(file);
       setFileUrl(url);
     } else {
-      alert("Please upload a PDF file.");
+      alert("Please upload a file.");
     }
   };
 
@@ -148,7 +148,7 @@ export default function Page() {
             ref={inputRef}
             type="file"
             onChange={handleChange}
-            accept="application/pdf"
+            // accept="application/pdf"
             className="hidden"
           />
         </div>
