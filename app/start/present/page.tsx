7e74d87a736b0ae7fea3a02177e1628211275cfa
Daryl Kevin <dkevin77@icloud.com>
2025-04-20 21:15:46 +0800

    fixing pdf width

--- app/start/present/page.tsx
+++ app/start/present/page.tsx
@@ -200,7 +200,7 @@ export default function Page() {
           >
             <div className="flex justify-center">
               {pdfFile ? (
-                <div className="h-[40vh] w-[75vw] overflow-auto rounded-md border border-gray-300 bg-gray-100">
+                <div className="h-[40vh] w-[80vw] overflow-auto rounded-md border border-gray-300 bg-gray-100">
                   <Document
                     file={pdfFile}
                     onLoadSuccess={(n) => setNumPages(n.numPages)}
