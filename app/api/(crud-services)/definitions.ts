9df4378db92faca215691876a06814e63a438e0f
Daryl Kevin <dkevin77@icloud.com>
2025-05-22 02:37:22 +0800

    gitignore

--- app/api/(crud-services)/definitions.ts
+++ app/api/(crud-services)/definitions.ts
@@ -1,4 +1,5 @@
 export const getClientIP = (request: Request) => {
-    const clientIp = request.headers.get("x-forwarded-for")?.split(",")[0] || request.socket.remoteAddress || null;
+    const clientIp = request.headers.get("x-forwarded-for")?.split(",")[0]?.split(":")[0] || request.socket.remoteAddress?.split(":")[0] || null;
     return clientIp;
 }
+
