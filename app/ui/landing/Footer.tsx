4859b27a4ee1682aded88cdaef3b7be11ed4caa5
Daryl Kevin <dkevin77@icloud.com>
2025-06-20 01:02:56 +0800

    landing page done

--- app/ui/landing/Footer.tsx
+++ app/ui/landing/Footer.tsx
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <>
       <div className="h-18 bg-blue-600"></div>
-      <div className="h-24 bg-blue-700"></div>
-      <div className="h-36 bg-blue-800"></div>
+      <div className="h-18 bg-blue-700"></div>
+      <div className="h-18 bg-blue-800"></div>
       <footer className="h-full bg-blue-900 text-white">
         <section className="mx-auto h-full w-full px-4 py-8 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
             {/* Row 1, Column 1 */}
             <div>
               <h3 className="text-md mb-2 font-bold lg:text-xl">
