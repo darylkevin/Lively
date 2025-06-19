1be369d4d9688433f490020ae6bd64a912921aa5
Daryl Kevin <dkevin77@icloud.com>
2025-06-19 23:17:26 +0800

    layouting

--- app/ui/landing/SubHero.tsx
+++ app/ui/landing/SubHero.tsx
@@ -5,7 +5,7 @@ const SubHero = () => {
   return (
     <div className="h-full overflow-x-hidden bg-blue-500">
       <section className="mx-auto flex flex-col items-center justify-center gap-8 py-12">
-        <div className="md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+        <div className="max-md:px-6 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
           <h1 className="text-center text-2xl font-bold text-white md:text-4xl xl:text-5xl">
             Powered by Microsoft's Azure AI Translator
           </h1>
@@ -26,7 +26,7 @@ const SubHero = () => {
                 <img
                   src={card.imageSrc}
                   alt={card.alt}
-                  className="mb-4 h-36 rounded-t-3xl object-cover lg:h-48 xl:h-64"
+                  className="mb-4 h-40 rounded-t-3xl object-cover lg:h-48 xl:h-64"
                 />
                 <div className="px-4 pb-2">
                   <h2 className="text-md font-semibold lg:text-xl">
@@ -46,7 +46,7 @@ const SubHero = () => {
                 <img
                   src={card.imageSrc}
                   alt={card.alt}
-                  className="mb-4 h-36 rounded-t-3xl object-cover lg:h-48 xl:h-64"
+                  className="mb-4 h-40 rounded-t-3xl object-cover lg:h-48 xl:h-64"
                 />
                 <div className="px-4 pb-2">
                   <h2 className="text-md font-semibold lg:text-xl">
